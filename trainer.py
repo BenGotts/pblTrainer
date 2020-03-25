77459c74093ff57e85aa9e79b563ae5a80593c33
BGottschalk <55898311+BGottschalk@users.noreply.github.com>
2020-03-25 14:08:18 -0700

    pbl trainer works. Gets exact cases from user

---
+++ trainer.py
@@ -0,0 +1,77 @@
+import json
+import random
+
+# random.seed()
+
+def getCases(topCases, bottomCases):
+
+    with open('algs.json') as f:
+        data = json.load(f)
+
+    trainingCases = []
+
+    for case in data['cases']:
+        # if(case['bottom'] in bottomCases):
+        #     trainingCases.append(case)
+        # #     print(case['top'], case['bottom'], case['alg'], "inv:", case['inv'])
+        # elif(case['top'] in topCases):
+        #     # print(case)
+        #     trainingCases.append(case)
+        #     # print(case['top'], case['bottom'], case['alg'], "inv:", case['inv'])
+        if((case['bottom'] in bottomCases) and (case['top'] in topCases)):
+            trainingCases.append(case)
+    # print(trainingCases, "\n")
+    f.close()
+    return trainingCases
+
+def getUserTopCases():
+    topCases = []
+    x = "yes"
+
+    print("Which cases on 'top' do you want? (enter one case at a time)")
+    while x != "no":
+        case = input("Enter a case: ")
+        topCases.append(case)
+        x = input("Another case? (yes/no): ")
+        if(x == "no"):
+            return topCases
+
+def getUserBottomCases():
+    bottomCases = []
+    x = "yes"
+
+    print("Which cases on 'bottom' do you want? (enter one case at a time)")
+    while x != "no":
+        case = input("Enter a case: ")
+        bottomCases.append(case)
+        x = input("Another case? (yes/no): ")
+        if(x == "no"):
+            return bottomCases
+
+def main():
+    print("PBL Trainer")
+    print("Choose specific cases or train all PBLs")
+    print("Valid inputs: A-, A+, adj, Ba, Bb, Ca, Cb, Da, Db, E, F, Ga, Gb, Gc, Gd, H, Ja, Jb, Ka, Kb, M, Na, Nb, O-, O+, opp, Pa, Pb, pJ, pN, Q, Ra, Rb, Sa, Sb, T, U-, U+, V, W, X, Y, Z, -")
+
+    x = input("Train all cases? (yes/no): ")
+    if(x == "no"):
+        topCases = getUserTopCases()
+        bottomCases = getUserBottomCases()
+    else:
+        topCases = ['A-', 'A+', 'adj', 'Ba', 'Bb', 'Ca', 'Cb', 'Da', 'Db', 'E', 'F', 'Ga', 'Gb', 'Gc', 'Gd', 'H', 'Ja', 'Jb', 'Ka', 'Kb', 'M', 'Na', 'Nb', 'O-', 'O+', 'opp', 'Pa', 'Pb', 'pJ', 'pN', 'Q', 'Ra', 'Rb', 'Sa', 'Sb', 'T', 'U-', 'U+', 'V', 'W', 'X', 'Y', 'Z', '-']
+        bottomCases = ['A-', 'A+', 'adj', 'Ba', 'Bb', 'Ca', 'Cb', 'Da', 'Db', 'E', 'F', 'Ga', 'Gb', 'Gc', 'Gd', 'H', 'Ja', 'Jb', 'Ka', 'Kb', 'M', 'Na', 'Nb', 'O-', 'O+', 'opp', 'Pa', 'Pb', 'pJ', 'pN', 'Q', 'Ra', 'Rb', 'Sa', 'Sb', 'T', 'U-', 'U+', 'V', 'W', 'X', 'Y', 'Z', '-']
+
+    trainingCases = getCases(topCases, bottomCases)
+    if(len(trainingCases) == 0):
+        print("\nTraining all cases, parity or bad input was detected")
+        topCases = ['A-', 'A+', 'adj', 'Ba', 'Bb', 'Ca', 'Cb', 'Da', 'Db', 'E', 'F', 'Ga', 'Gb', 'Gc', 'Gd', 'H', 'Ja', 'Jb', 'Ka', 'Kb', 'M', 'Na', 'Nb', 'O-', 'O+', 'opp', 'Pa', 'Pb', 'pJ', 'pN', 'Q', 'Ra', 'Rb', 'Sa', 'Sb', 'T', 'U-', 'U+', 'V', 'W', 'X', 'Y', 'Z', '-']
+        bottomCases = ['A-', 'A+', 'adj', 'Ba', 'Bb', 'Ca', 'Cb', 'Da', 'Db', 'E', 'F', 'Ga', 'Gb', 'Gc', 'Gd', 'H', 'Ja', 'Jb', 'Ka', 'Kb', 'M', 'Na', 'Nb', 'O-', 'O+', 'opp', 'Pa', 'Pb', 'pJ', 'pN', 'Q', 'Ra', 'Rb', 'Sa', 'Sb', 'T', 'U-', 'U+', 'V', 'W', 'X', 'Y', 'Z', '-']
+        trainingCases = getCases(topCases, bottomCases)
+
+    x = 'yes'
+    while(x == 'yes'):
+        randCase = trainingCases[random.randint(0,len(trainingCases))]
+        print("\n(" + randCase['top'], randCase['bottom'] + ")", randCase['inv'])
+        x = input("Another case? (yes/no): ")
+
+main()
